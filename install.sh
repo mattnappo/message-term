0576c3e0fbe3ba605f1620f0aaaa2d5f681d0aed
xoreo <mattnappo@gmail.com>
2018-09-03 15:13:10 -0400

    updated install script

--- install.sh
+++ install.sh
@@ -1,18 +1,2 @@
-if [ -d ~/bin ]   # for file "if [-f /home/rama/file]" 
-then 
-    echo "bin present"
-else
-    mkdir ~/bin
-    echo "bin created"
-fi
-
-cp MessageRecieve.applescript ~/Library/Application\ Scripts/com.apple.iChat
-echo "copied MessageRecieve.applescript to Library"
-
-cp SendMessage.sh ~/bin
-echo "copied SendMessage.sh to bin"
-
-cp bin/handler ~/bin
-echo "copied handler to bin"
-
-echo "done"
+cp src/Handler.applescript ~/Library/Application\ Scripts/com.apple.iChat
+echo "installation complete"
