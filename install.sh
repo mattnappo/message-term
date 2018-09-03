9080b320b39ec73de37342b3998582f6b17298c9
xoreo <mattnappo@gmail.com>
2018-09-03 15:18:30 -0400

    made install script better

--- install.sh
+++ install.sh
@@ -1,2 +1,20 @@
+if [ -d ./bin ]   # for file "if [-f /home/rama/file]" 
+then 
+    echo "./bin present"
+else
+    mkdir ./bin
+    echo "./bin created"
+fi
+make handler
+echo "built handler"
+if [ -d ~/bin ]   # for file "if [-f /home/rama/file]" 
+then 
+    echo "~/bin present"
+else
+    mkdir ~/bin
+    echo "~/bin created"
+fi
 cp src/Handler.applescript ~/Library/Application\ Scripts/com.apple.iChat
-echo "installation complete"
+echo "Installed AppleScript Handler"
+cp bin/handler ~/bin
+echo "Installed Main Handler"
