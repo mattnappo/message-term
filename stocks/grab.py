6bc3e42ec3b829901f246bb82ddf338c7327452c
xoreo <mattnappo@gmail.com>
2018-09-10 11:22:44 -0400

    added grabber

---
+++ stocks/grab.py
@@ -0,0 +1,44 @@
+import requests
+import sys
+import json
+from os import system as bash
+
+class Grab:
+    def __init__(self, api_key, message, number):
+        self.data = None
+        self.ticker = ""
+
+        self.api_key = api_key
+        self.message = message
+        self.number = number
+
+        self.handle()
+        
+    def handle(self):
+        words = self.message.lower().split(" ")
+        if words[0] == "price":
+            self.ticker = words[1]
+
+        self.get_current_price()
+
+    def get_current_price(self):
+        request = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + self.ticker + "&apikey=" + self.api_key
+        self.data = requests.get(request)
+        self.data = self.data.json()
+        self.parse()
+
+    def parse(self):
+        if self.data["Global Quote"] != {}:
+            self.ticker = self.ticker.upper()
+            raw_price = self.data["Global Quote"]["05. price"]
+            money = raw_price[:-2]
+            price = "Price of " + self.ticker + ": \$" + money
+            bash("~/.msgterm/message_term --send " + self.number + " '" + price + "'")
+        else:
+            bash("~/.msgterm/message_term --send " + self.number + " \"Oops. It looks like the ticker '" + self.ticker.upper() + "' is invalid!\"")
+
+Grab(
+    "GDJIXVNNXQGHSD7A",
+    sys.argv[1],
+    "9144142874"
+)
