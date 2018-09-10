156145659cb549479d14810a663e7099e58bb751
xoreo <mattnappo@gmail.com>
2018-09-09 23:11:04 -0400

    basic stock loopup working

--- stocks/grab.py
+++ stocks/grab.py
@@ -12,11 +12,18 @@ class Grab:
     def get_current_price(self):
         request = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + self.ticker + "&apikey=" + self.key
         self.data = requests.get(request)
+        self.data = self.data.json()
+        self.parse()
+
+    def parse(self):
+        raw_price = self.data["Global Quote"]["05. price"]
+        money = raw_price.split(".")
+        dollars = money[0]
+        cents = money[1][:2]
+        print("Price of " + self.ticker + ": $" + dollars + "." + cents)
+
 
 g = Grab(
     "GDJIXVNNXQGHSD7A",
     sys.argv[1]
 )
-
-
-print(g.data["Global Quote"]["05. price"])
