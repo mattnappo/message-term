60f57399e9adaa065578a44cc6994c510d49fdcf
xoreo <mattnappo@gmail.com>
2018-10-05 13:35:21 -0400

    Adding multiple windows

--- src/main.js
+++ src/main.js
@@ -2,35 +2,76 @@ const imessage = require("osa-imessage");
 var blessed = require('blessed');
 
 var screen;
-var messages;
+var message_window;
+var chat_window;
 
 function init_scr() {
     screen = blessed.screen({
         smartCSR: true
     });
-    screen.title = 'MessageTerm';
+    screen.title = 'Message Term';
 
     screen.key(['C-x'], function(ch, key) {
         return process.exit(0);
     });
 
-    var box = blessed.box({
-        top: 0,
-        
+    var header = blessed.box({
         width: '100%',
-        height: '5%',
-        content: 'Message Term',
+        content: '{center}Message Term{/center}',
         tags: true,
+        border: {
+            type: 'line'
+        },
         style: {
+            border: {
+                fg: '#f0f0f0'
+            },
             fg: 'black',
-            bg: 'magenta',
-
-            hover: {
-                bg: 'green'
-            }
+            bg: 'magenta'
         }
     });
-    screen.append(box);
+
+    message_window = blessed.box({
+        top: "5%",
+        width: '50%',
+        height: "100%",
+        content: '{center}Messages{/center}',
+        tags: true,
+        border: {
+            type: 'line'
+        },
+        style: {
+            border: {
+                fg: '#f0f0f0'
+            },
+            fg: 'black',
+            bg: 'magenta'
+        }
+    });
+
+    chat_window = blessed.box({
+        top: "5%",
+        left: "50%",
+        width: '50%',
+        height: "100%",
+        content: '{center}Chats{/center}',
+        tags: true,
+        border: {
+            type: 'line'
+        },
+        style: {
+            border: {
+                fg: '#f0f0f0'
+            },
+            fg: 'black',
+            bg: 'magenta'
+        }
+    });
+
+    screen.append(header);
+    screen.append(message_window);
+    screen.append(chat_window);
+    
 }
 
 function incoming_message(message, sender) {
@@ -39,32 +80,34 @@ function incoming_message(message, sender) {
         sender: sender
     };
 
-    var box = blessed.box({
-        top: "center",
-        
-        width: '100%',
-        height: '5%',
-        content: msg.sender,
-        tags: true,
-        style: {
-            fg: 'white',
-            bg: 'magenta',
-            border: {
-                fg: '#f0f0f0'
-            },
-            hover: {
-                bg: 'green'
-            }
-        }
-    });
-    screen.append(box);
+    // var box = blessed.box({
+    //     top: "center",
 
-    box.on('click', function(data) {
-        box.setContent(msg.message);
-        screen.render();
-    });
+    //     width: '100%',
+    //     height: '5%',
+    //     content: msg.sender,
+    //     tags: true,
+    //     style: {
+    //         fg: 'white',
+    //         bg: 'magenta',
+    //         border: {
 
-    box.focus();
+    //             fg: '#f0f0f0'
+    //         },
+    //         hover: {
+    //             bg: 'green'
+    //         }
+    //     }
+    // });
+    // screen.append(box);
+
+    // box.on('click', function(data) {
+    //     box.setContent(msg.message);
+    //     screen.render();
+    // });
+
+    // box.focus();
+    return msg;
 
 }
 
@@ -85,9 +128,6 @@ imessage.getRecentChats(20);
 
 console.log(imessage.getRecentChats(20));
 
-imessage.handleForName("Charlie Loigman").then(handle => {
-    imessage.send(handle, "gn");
-});
 
 
 
