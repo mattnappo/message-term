2fe95676bbbcccb757a7b3116f7b79f358044652
xoreo <mattnappo@gmail.com>
2018-10-05 17:21:17 -0400

    improved UI

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// ----- INIT CODE -----
+
 const imessage = require("osa-imessage");
 var blessed = require("blessed");
 
@@ -5,14 +7,26 @@ var screen;
 var message_window;
 var chat_window;
 
+var no_messages;
+var no_chats;
+
 var settings = {
     foreground: "#45ff30",
     background: "black"
 };
 
+var message_count = 0;
+
+// ----- SETUP -----
+
+process.on("unhandledRejection", error => {
+    console.log("unhandledRejection", error.message);
+});
+
 function init_scr() {
     screen = blessed.screen({
-        smartCSR: true
+        smartCSR: true,
+        debug: true
     });
     screen.title = "Message Term";
 
@@ -42,7 +56,7 @@ function init_scr() {
         top: 3,
         width: "50%",
         height: "100%",
-        content: "{center}{bold}Messages{/bold}{/center}",
+        label: "{" + settings.foreground + "-fg}{bold}Messages{/bold}",
         tags: true,
         border: {
             type: "line"
@@ -61,7 +75,7 @@ function init_scr() {
         left: "50%",
         width: "50%",
         height: "100%",
-        content: "{center}{bold}Chats{/bold}{/center}",
+        label: "{" + settings.foreground + "-fg}{bold}Chats{/bold}",
         tags: true,
         border: {
             type: "line"
@@ -75,18 +89,64 @@ function init_scr() {
         }
     });
 
+    if (message_count <= 0) {
+        var no_messages = blessed.box({
+            parent: message_window,
+            left: "center",
+            top: "center",
+            height: 3,
+            width: "90%",
+            content: "{center}No new messages{/center}",
+            tags: true,
+            border: {
+                type: "line"
+            },
+            style: {
+                border: {
+                    fg: settings.foreground
+                },
+                fg: settings.foreground,
+                bg: settings.background
+            }
+        });
+
+        var no_chats = blessed.box({
+            parent: chat_window,
+            left: "center",
+            top: "center",
+            height: 3,
+            width: "90%",
+            content: "{center}No current chats{/center}",
+            tags: true,
+            border: {
+                type: "line"
+            },
+            style: {
+                border: {
+                    fg: settings.foreground
+                },
+                fg: settings.foreground,
+                bg: settings.background
+            }
+        });
+    }
+    
     screen.append(header);
     screen.append(message_window);
     screen.append(chat_window);
-    
+}
 
+// ----- HANDLING -----
+
+function open_chat() {
+    console.log("chat opened");
 }
 
 function incoming_message(message, sender) {
     var new_message = blessed.box({
         parent: message_window,
         left: "center",
-        top: 1,
+        top: 1 + message_count * 3,
         height: 3,
         width: "90%",
         content: "{center}" + sender + "{/center}",
@@ -102,33 +162,29 @@ function incoming_message(message, sender) {
             bg: settings.background
         }
     });
-
-    new_message.on('click', function(data) {
-        new_message.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
+    
+    new_message.on("click", function(data) {
+        open_chat();
         screen.render();
     });
-
+    screen.render();
 }
+
+// ----- MAIN CODE -----
 
 init_scr();
 
-process.on("unhandledRejection", error => {
-    console.log("unhandledRejection", error.message);
-});
-incoming_message("asdad", "sender");
+incoming_message("my message\nhi", "sender");
+message_count += 1;
+
 imessage.listen().on("message", (msg) => {
-    if (!msg.fromMe) {
-        incoming_message(msg.text, msg.handle);
-        // console.log(`"${msg.text}" from ${msg.handle}`);
-    }
+    var name_object = imessage.nameForHandle(msg.handle);
+    message_count += 1;
+    name_object.then(function(result) {
+        incoming_message(msg.text, result);
+        
+    });
+    
 });
 
-imessage.getRecentChats(20);
-
-console.log(imessage.getRecentChats(20));
-
-
-
-
-// Render the screen.
 screen.render();
