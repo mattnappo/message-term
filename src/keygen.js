c5c0aa633d68f556d6dfc905dc05edbc7dce3227
xoreo <mattnappo@gmail.com>
2018-10-22 20:52:58 -0400

    Basic keygen script

--- src/keygen.js
+++ src/keygen.js
@@ -1,12 +1,30 @@
+const r_crypto = require("crypto");
 const crypto = require("./crypto.js");
-const m_path = require("path");
+const path = require("path");
+const fs = require("fs");
 
-var path = m_path.resolve(__dirname, "..", "key", "public.pem");
-console.log(path)
+function random_hash() {
+    var md5 = r_crypto.createHash("md5");
+    var raw = Math.random().toString() + Math.random().toString() + (new Date).getTime().toString();
+    var raw_hash = md5.update(raw);
+    var digested_hash = raw_hash.digest("hex");
+    return digested_hash.substring(0, 8);
+}
 
-// var fs = require('fs');
-// var dir = './tmp';
+function create_dir(dir) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+}
 
-// if (!fs.existsSync(dir)){
-//     fs.mkdirSync(dir);
-// }
+function main() {
+    var root = path.resolve(__dirname, "..", "keys");
+    create_dir(root);
+    
+    var name = random_hash();
+    var this_key = path.resolve(__dirname, "..", "keys", name);
+    create_dir(this_key);
+
+}
+
+main();
