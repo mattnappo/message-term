ea268a2e029e9fc9a3606999ef34a624c0b7a86c
xoreo <mattnappo@gmail.com>
2019-02-08 19:14:17 -0500

    Reviving this proj

--- src/main.js
+++ src/main.js
@@ -1,21 +1,8 @@
 // ----- INIT CODE -----
 const imessage = require("osa-imessage")
 const blessed = require("blessed")
-const crypto = require("./crypto")
-
-const fs = require("fs")
-const path = require("path")
 
 var windows = require("./components").windows
-
-var master_keys = {
-    "keys": {
-
-    }
-}
-
-var public_path
-var private_path
 
 var no_messages
 var no_chats
@@ -28,7 +15,6 @@ var settings = {
 }
 
 var current_chat = ""
-var pending_send = { }
 
 var message_count = 0
 var clicked_chat = false
@@ -50,43 +36,6 @@ process.on("uncaughtException", function (err) {
 function hide_element(element) {
     element.content = ""
     element.border.type = "none"
-}
-
-function show_key_error() {
-    var errorbox = blessed.box({
-        parent: windows.screen,
-        top: "25%",
-        left: "25%",
-        width: "50%",
-        height: "50%",
-        tags: true,
-        border: {
-            type: "line"
-        },
-        style: {
-            border: {
-                fg: settings.foreground
-            },
-            fg: settings.foreground,
-            bg: settings.background
-        }
-    })
-
-    var errorbox = blessed.box({
-        parent: windows.screen,
-        top: "50%",
-        left: "25%",
-        width: "50%",
-        height: "50%",
-        tags: true,
-        content: "{center}Run 'npm run keymanage' to generate a key!{/center}",
-        style: {
-            fg: settings.foreground,
-            bg: settings.background
-        }
-    })
-    
-    windows.screen.render()
 }
 
 // ----- HANDLERS -----
@@ -115,38 +64,18 @@ windows.compose_window.on("submit", function(data) {
 })
 
 windows.input_window.on("submit", function(data) {
-    if (current_chat != "") {
-        var message = windows.input_box.getContent()
-        if (message != "") {
-            windows.input_window.reset()
-            if (!(current_chat in master_keys["keys"])) {
-                fs.readFile(public_path, {encoding: 'utf-8'}, function(err, public_key) {
-                    if (err) console.log(err)
-                    var escaped = public_key.replace(/\n/g, String.raw`\n`)
-                    var r = {
-                        "public_key": escaped,
-                        "body": "--REQUEST PUBLIC KEY--"
-                    }
-                    pending_send = {
-                        to: current_chat,
-                        message: message
-                    }
-                    send_message(current_chat, JSON.stringify(r))
-                })
-            } else {
-                forge_message(current_chat, message, true)
-                var public_key = master_keys["keys"][current_chat]
-                var escaped = public_key.replace(/\n/g, String.raw`\n`)
-                var r = {
-                    "public_key": escaped,
-                    "body": crypto.encrypt_k(message, public_key)
-                }
-                send_message(current_chat, JSON.stringify(r))
-                
-            }
-            windows.input_box.focus()
-        }
+    var content = windows.compose_window.getContent()
+    if (content != "") {
+        current_chat = content
     }
+    var message = windows.input_box.getContent()
+    if (message != "") {
+        windows.input_window.reset()
+        forge_message(current_chat, message, true)
+        send_message(current_chat, message)
+        windows.input_box.focus()
+    }
+    
 })
 
 // ----- HANDLERS -----
@@ -303,18 +232,7 @@ function count(obj) {
 
 // ----- MAIN CODE -----
 
-try {
-    var p = path.resolve(__dirname, "..", "keys", "keys.json")
-    var raw = fs.readFileSync(p, "utf8")
-    var contents = JSON.parse(raw)
-    var mainkey = contents["mainkey"]
-        
-    public_path = path.resolve(__dirname, "..", "keys", mainkey, "public.pem")
-    private_path = path.resolve(__dirname, "..", "keys", mainkey, "private.pem")
-    init_scr()
-} catch (err) {
-    show_key_error()
-}
+init_scr()
 
 function forge_message(name, message, to_recipient) {
     message_count += 1
@@ -333,7 +251,7 @@ function forge_message(name, message, to_recipient) {
     
     if (to_recipient) {
         conversation[len].color = settings.blue
-        conversation[len].left = "53%"
+        conversation[len].left = windows.screen.width / 2 + 3
     } else {
         conversation[len].color = settings.white
     }
@@ -344,48 +262,13 @@ function forge_message(name, message, to_recipient) {
 
 imessage.listen().on("message", (msg) => {
     // if (!msg.fromMe) {
-        fs.readFile(private_path, {encoding: 'utf-8'}, function(err, private_key) {
-            if (err) console.log(err)
-            var name_object = imessage.nameForHandle(msg.handle)
-            try {
-                var decoded_r = JSON.parse(msg.text)
-                var body = decoded_r["body"]
-                var their_public = decoded_r["public_key"]
-                if (body == "--REQUEST PUBLIC KEY--") {
-                    fs.readFile(public_path, {encoding: 'utf-8'}, function(err, public_key) {
-                        if (err) console.log(err)
-                        var escaped = public_key.replace(/\n/g, String.raw`\n`)
-                        var r = {
-                            "public_key": escaped,
-                            "body": "--INCOMING PUBLIC KEY--"
-                        }
-                        send_message(current_chat, JSON.stringify(r))
-                    })
-                } else if (body == "--INCOMING PUBLIC KEY--") {
-                    name_object.then(function(name) {
-                        master_keys["keys"][name] = their_public
-                        fs.readFile(public_path, {encoding: 'utf-8'}, function(err, public_key) {
-                            if (err) console.log(err)
-                            var escaped = public_key.replace(/\n/g, String.raw`\n`)
-                            var r = {
-                                "public_key": escaped,
-                                "body": crypto.encrypt_k(pending_send[current_chat], their_public)
-                            }
-                            send_message(current_chat, JSON.stringify(r))
-                        })
-                    })
-                } else {
-                    var decrypted = crypto.decrypt_k(body, private_key)
-                    name_object.then(function(name) {
-                        forge_message(name, decrypted)
-                        windows.screen.render()
-                    })
-                }
-            } catch (err) { }
-        })
+    var name_object = imessage.nameForHandle(msg.handle)
+    name_object.then(function(name) {
+        forge_message(name, msg.text)
+        windows.screen.render()
+    })
     // }
 })
 
 windows.input_window.focus()
-
 windows.screen.render()
